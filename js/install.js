0c6febe3fbadb62562c7240ec08ec573c0ef7179
stronganchor <77190959+stronganchor@users.noreply.github.com>
2024-12-02 16:06:53 +0300

    Update install.js

--- js/install.js
+++ js/install.js
@@ -6,19 +6,28 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
     const notification = document.getElementById('pwa-install-notification');
     const installBtn = document.getElementById('pwa-install-btn');
+    const dismissBtn = document.getElementById('pwa-dismiss-btn');
 
     notification.style.display = 'block';
+
+    installBtn.textContent = pwaInstallVars.installText;
+    dismissBtn.textContent = pwaInstallVars.dismissText;
 
     installBtn.addEventListener('click', () => {
         notification.style.display = 'none';
         deferredPrompt.prompt();
+
         deferredPrompt.userChoice.then(choiceResult => {
             if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the install prompt');
-            } else {
-                console.log('User dismissed the install prompt');
+                console.log('PWA installed');
             }
             deferredPrompt = null;
+        }).catch(err => {
+            console.error('Error handling install prompt:', err);
         });
+    });
+
+    dismissBtn.addEventListener('click', () => {
+        notification.style.display = 'none';
     });
 });
